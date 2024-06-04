3762fc6ed54e41f683ddda008f7643acc065f64c
JeongHyun Ha <aszx2024@naver.com>
2024-06-04 18:08:37 +0900

    오류 수정

--- frontend/src/page/store/StoreList.jsx
+++ frontend/src/page/store/StoreList.jsx
@@ -1,5 +1,5 @@
 import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
