c42c4771fb6dd64371d41efc93bc9f8548dfe30b
JeongHyun Ha <aszx2024@naver.com>
2024-06-17 18:46:31 +0900

    폰트 수정

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -2,19 +2,19 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
-    heading: "'Pretendard-Regular', sans-serif",
-    body: "'Pretendard-Regular', sans-serif",
+    heading: "'Freesentation-9Black', sans-serif",
+    body: "'Freesentation-9Black', sans-serif",
   },
   styles: {
     global: {
       "@font-face": {
-        fontFamily: "Pretendard-Regular",
-        src: "url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')",
+        fontFamily: "Freesentation-9Black",
+        src: "url('https://fastly.jsdelivr.net/gh/projectnoonnu/2404@1.0/Freesentation-9Black.woff2') format('woff')",
         fontWeight: "400",
         fontStyle: "normal",
       },
       body: {
-        fontFamily: "'Pretendard-Regular', sans-serif",
+        fontFamily: "'Freesentation-9Black', sans-serif",
       },
     },
   },
