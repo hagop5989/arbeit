2794077621e464a7dc566bf728ece688e9430230
JeongHyun Ha <aszx2024@naver.com>
2024-06-18 16:10:00 +0900

    로그인 디자인 완료

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,23 +1,54 @@
 import {
   Box,
   Button,
+  Flex,
   FormControl,
-  FormHelperText,
   FormLabel,
   Heading,
   Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  Switch,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   useToast,
 } from "@chakra-ui/react";
 import { useContext, useState } from "react";
 import axios from "axios";
 import { LoginContext } from "../component/LoginProvider.jsx";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
+
+const styles = {
+  formControl: {
+    marginBottom: "60px",
+    height: "100px",
+  },
+  formLabel: {
+    fontSize: "25px",
+    width: "100%",
+    borderBottom: "2px solid #1F3042",
+    marginBottom: 6,
+  },
+  center: {
+    width: "97%",
+    margin: "auto",
+    display: "block",
+  },
+};
 
 export function Login() {
   const [authority, setAuthority] = useState("ALBA");
   const [email, setEmail] = useState("");
   const [errors, setErrors] = useState({});
   const [password, setPassword] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
   const toast = useToast();
   const navigate = useNavigate();
 
@@ -36,12 +67,10 @@ export function Login() {
         });
       })
       .catch((err) => {
-        if (err.response.status === 403) {
-          toast({
-            status: "warning",
-            description: "권한을 확인해주세요.",
-            position: "top",
-          });
+        if (err.response.status === 400) {
+          alert("아이디와 비밀번호를 확인해주세요.");
+        } else if (err.response.status === 404) {
+          alert("일치하는 정보가 없습니다.");
         }
         setErrors(err.response.data);
       })
@@ -52,44 +81,154 @@ export function Login() {
     setAuthority(value);
   }
 
+  function handleSwitchChange() {
+    if (isAdmin === true) {
+      setIsAdmin(false);
+      setAuthority("BOSS");
+    } else {
+      setIsAdmin(true);
+      setAuthority("ADMIN");
+    }
+  }
+
+  const isError = (prop) => prop !== undefined;
+
+  function handleAuthTab(auth) {
+    setAuthority(auth);
+  }
+
   return (
-    <Box>
-      <Box>
-        <Heading>로그인</Heading>
+    <Box w={"500px"} h={"500px"}>
+      <Box
+        h={"60px"}
+        mb={"30px"}
+        bg={"#FF7F3E"}
+        color={"white"}
+        borderRadius={"10px"}
+      >
+        <Heading size={"lg"} textAlign={"center"} lineHeight={"60px"}>
+          로그인
+        </Heading>
       </Box>
       <Box>
-        <Box>
-          <Button
-            colorScheme={authority === "ALBA" ? "blue" : "gray"}
-            onClick={() => handleTypeBtn("ALBA")}
-          >
-            알바
-          </Button>
-          <Button
-            colorScheme={authority === "BOSS" ? "blue" : "gray"}
-            onClick={() => handleTypeBtn("BOSS")}
-          >
-            사장
-          </Button>
-          <Button
-            colorScheme={authority === "ADMIN" ? "blue" : "gray"}
-            onClick={() => handleTypeBtn("ADMIN")}
-          >
-            관리자 (나중에 지울 예정)
-          </Button>
-        </Box>
-        <Box>
-          <FormControl>
-            <FormLabel>이메일</FormLabel>
-            <Input onChange={(e) => setEmail(e.target.value)} />
-            {errors && <FormHelperText>{errors.email}</FormHelperText>}
-            <FormLabel>비밀번호</FormLabel>
-            <Input onChange={(e) => setPassword(e.target.value)} />
-            {errors && <FormHelperText>{errors.password}</FormHelperText>}
-          </FormControl>
-        </Box>
-        <Box>
-          <Button onClick={handleLoginBtn}>로그인</Button>
+        <Tabs isFitted position="relative" variant="unstyled">
+          <TabList>
+            <Tab onClick={() => handleAuthTab("ALBA")}>알바</Tab>
+            <Tab onClick={() => handleAuthTab("BOSS")}>기업</Tab>
+          </TabList>
+          <TabIndicator mt="-1.5px" height="2px" bg="#FF7F3E" />
+          <TabPanels>
+            <TabPanel display={"flex"}>
+              <Box w={"350px"} h={"100%"}>
+                <FormControl isInvalid={isError(errors.email)} mt={"20px"}>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none" color={"gray.400"}>
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </InputLeftElement>
+                    <Input
+                      defaultValue={email}
+                      type="email"
+                      placeholder="이메일"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl isInvalid={isError(errors.password)} mt={"5px"}>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none" color={"gray.400"}>
+                      <FontAwesomeIcon icon={faKey} />
+                    </InputLeftElement>
+                    <Input
+                      defaultValue={password}
+                      type="text"
+                      placeholder="패스워드"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </InputGroup>
+                </FormControl>
+              </Box>
+              <Box ml={3} mt={"19px"}>
+                <Button
+                  w={"100px"}
+                  h={"89px"}
+                  onClick={handleLoginBtn}
+                  colorScheme={"orange"}
+                >
+                  로그인
+                </Button>
+              </Box>
+            </TabPanel>
+            <TabPanel>
+              <Box>
+                <FormControl display="flex" alignItems="center" mb="15px">
+                  <FormLabel
+                    htmlFor="isAdmin"
+                    mb={"0"}
+                    color={isAdmin ? "black" : "gray"}
+                  >
+                    관리자입니까?
+                  </FormLabel>
+                  <Switch
+                    id="isAdmin"
+                    onChange={handleSwitchChange}
+                    isChecked={isAdmin}
+                  />
+                </FormControl>
+                <Flex>
+                  <Box w={"350px"} h={"100%"}>
+                    <FormControl isInvalid={isError(errors.email)} mt={"20px"}>
+                      <InputGroup>
+                        <InputLeftElement
+                          pointerEvents="none"
+                          color={"gray.400"}
+                        >
+                          <FontAwesomeIcon icon={faEnvelope} />
+                        </InputLeftElement>
+                        <Input
+                          defaultValue={email}
+                          type="email"
+                          placeholder="이메일"
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </InputGroup>
+                    </FormControl>
+                    <FormControl
+                      isInvalid={isError(errors.password)}
+                      mt={"5px"}
+                    >
+                      <InputGroup>
+                        <InputLeftElement
+                          pointerEvents="none"
+                          color={"gray.400"}
+                        >
+                          <FontAwesomeIcon icon={faKey} />
+                        </InputLeftElement>
+                        <Input
+                          defaultValue={password}
+                          type="text"
+                          placeholder="패스워드"
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </InputGroup>
+                    </FormControl>
+                  </Box>
+                  <Box ml={3} mt={"19px"}>
+                    <Button
+                      w={"100px"}
+                      h={"89px"}
+                      onClick={handleLoginBtn}
+                      colorScheme={"orange"}
+                    >
+                      로그인
+                    </Button>
+                  </Box>
+                </Flex>
+              </Box>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+        <Box mt={"20px"} ml={"20px"} color={"gray.600"}>
+          <Link href={"/signup"}>회원가입</Link>
         </Box>
       </Box>
     </Box>
